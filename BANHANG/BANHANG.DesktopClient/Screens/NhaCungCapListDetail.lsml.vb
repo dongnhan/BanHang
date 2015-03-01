42f87a4abdf9749ed018531f923f3a47ee0f116c
lekhanhky <kykhanh2003@gmail.com>
2015-03-01 15:40:29 +0700

    nhà cung cấp !

---
+++ BANHANG/BANHANG.DesktopClient/Screens/NhaCungCapListDetail.lsml.vb
@@ -0,0 +1,19 @@
+﻿
+Namespace LightSwitchApplication
+
+    Public Class NhaCungCapListDetail
+
+        Private Sub Đối_TượngItemListAddAndEditNew_CanExecute(ByRef result As Boolean)
+            ' Write your code here.
+
+        End Sub
+
+        Private Sub Đối_TượngItemListAddAndEditNew_Execute()
+            Me.NhaCungCap.AddNew() ' Write your code here.
+            Me.NhaCungCap.SelectedItem.Là_Khách_Hàng = False
+            Me.NhaCungCap.SelectedItem.Là_Nhà_Cung_Cấp = True
+            Me.FindControl("Tên1").Focus()
+        End Sub
+    End Class
+
+End Namespace
