6ab6504c6ad873954386eb232545737b519caa9b
lekhanhky <kykhanh2003@gmail.com>
2015-03-01 16:39:02 +0700

    chi tiền mặt

---
+++ BANHANG/BANHANG.DesktopClient/Screens/ChisListDetail.lsml.vb
@@ -0,0 +1,56 @@
+﻿
+Namespace LightSwitchApplication
+
+    Public Class ChisListDetail
+        Dim flagEdit As Boolean
+        Private Sub ChiListAddAndEditNew_CanExecute(ByRef result As Boolean)
+            ' Write your code here.
+
+        End Sub
+
+        Private Sub ChiListAddAndEditNew_Execute()
+            ' Write your code here.
+            Me.Chis.AddNew()
+            Me.FindControl("Đối_TượngItem1").Focus()
+        End Sub
+
+        Private Sub OK_Execute()
+            ' Write your code here.
+            Me.CloseModalWindow("GroupChiTiet")
+        End Sub
+
+        Private Sub Hủy_Execute()
+            ' Write your code here.
+            If flagEdit Then
+                CType(Me.Chi_Chi_Tiết.SelectedItem, ChiChiTiet).Details.DiscardChanges()
+            Else
+                Me.Chi_Chi_Tiết.DeleteSelected()
+            End If
+            Me.CloseModalWindow("GroupChiTiet")
+        End Sub
+
+        Private Sub Chi_Chi_TiếtAddAndEditNew_CanExecute(ByRef result As Boolean)
+            ' Write your code here.
+
+        End Sub
+
+        Private Sub Chi_Chi_TiếtAddAndEditNew_Execute()
+            ' Write your code here.
+            Me.Chi_Chi_Tiết.AddNew()
+            Me.OpenModalWindow("GroupChiTiet") ' Write your code here.
+            Me.flagEdit = False
+        End Sub
+
+        Private Sub Chi_Chi_TiếtEditSelected_CanExecute(ByRef result As Boolean)
+            ' Write your code here.
+
+        End Sub
+
+        Private Sub Chi_Chi_TiếtEditSelected_Execute()
+            ' Write your code here.
+            Me.OpenModalWindow("GroupChiTiet") ' Write your code here.
+            Me.flagEdit = True
+        End Sub
+    End Class
+
+End Namespace
---
+++ BANHANG/BANHANG.Server/DataSources/BanHangData/Chi.lsml.vb
@@ -0,0 +1,18 @@
+﻿
+Namespace LightSwitchApplication
+
+    Public Class Chi
+
+        Private Sub Số_tiền_Validate(results As EntityValidationResultsBuilder)
+            Dim tien = 0.0
+            For Each d In Chi_Chi_Tiết
+                tien += d.Số_tiền
+            Next
+            If tien > 0 Then
+                Me.Số_tiền = tien
+            End If ' results.AddPropertyError("<Error-Message>")
+
+        End Sub
+    End Class
+
+End Namespace
