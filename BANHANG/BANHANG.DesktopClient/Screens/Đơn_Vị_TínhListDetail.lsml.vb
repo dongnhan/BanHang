80c6b4adfeda40bd1588fc5e7bf20432f279ae11
lekhanhky <kykhanh2003@gmail.com>
2015-03-01 15:58:32 +0700

    Đơn vị tính

---
+++ BANHANG/BANHANG.DesktopClient/Screens/Đơn_Vị_TínhListDetail.lsml.vb
@@ -0,0 +1,17 @@
+﻿
+Namespace LightSwitchApplication
+
+    Public Class Đơn_Vị_TínhListDetail
+
+        Private Sub Đơn_Vị_TínhItemListAddAndEditNew_CanExecute(ByRef result As Boolean)
+            ' Write your code here.
+
+        End Sub
+
+        Private Sub Đơn_Vị_TínhItemListAddAndEditNew_Execute()
+            Me.Đơn_Vị_Tính.AddNew() ' Write your code here.
+            Me.FindControl("Tên1").Focus()
+        End Sub
+    End Class
+
+End Namespace
