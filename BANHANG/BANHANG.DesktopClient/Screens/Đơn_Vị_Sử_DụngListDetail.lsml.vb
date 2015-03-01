93ae6afac8b412c6058eda2e1746d26563fe2734
lekhanhky <kykhanh2003@gmail.com>
2015-03-01 15:55:24 +0700

    đơn vị sử dụng (thông tin ban đầu)

---
+++ BANHANG/BANHANG.DesktopClient/Screens/Đơn_Vị_Sử_DụngListDetail.lsml.vb
@@ -0,0 +1,18 @@
+﻿
+Namespace LightSwitchApplication
+
+    Public Class Đơn_Vị_Sử_DụngListDetail
+
+       
+        Private Sub Đơn_Vị_Sử_DụngListDetail_Activated()
+            If Đơn_Vị_Sử_Dụng.SelectedItem IsNot Nothing Then
+
+            Else
+                Me.Đơn_Vị_Sử_Dụng.AddNew()
+                Me.FindControl("Tên1").Focus()
+            End If
+            ' Write your code here.
+        End Sub
+    End Class
+
+End Namespace
