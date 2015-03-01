0ee98999c9852c0fc48c11f21fbb33e6a9a27aa4
lekhanhky <kykhanh2003@gmail.com>
2015-03-01 15:30:29 +0700

    khách hàng

---
+++ BANHANG/BANHANG.DesktopClient/Screens/KhachHangListDetail.lsml.vb
@@ -0,0 +1,19 @@
+﻿
+Namespace LightSwitchApplication
+
+    Public Class KhachHangListDetail
+
+        Private Sub Đối_TượngItemListAddAndEditNew_CanExecute(ByRef result As Boolean)
+            ' Write your code here.
+
+        End Sub
+
+        Private Sub Đối_TượngItemListAddAndEditNew_Execute()
+            Me.KhachHang.AddNew()
+            Me.KhachHang.SelectedItem.Là_Khách_Hàng = True
+            Me.KhachHang.SelectedItem.Là_Nhà_Cung_Cấp = False
+            Me.FindControl("Tên1").Focus()
+        End Sub
+    End Class
+
+End Namespace
