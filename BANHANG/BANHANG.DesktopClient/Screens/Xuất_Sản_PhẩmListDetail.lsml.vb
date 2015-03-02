947138f438717a4c2244aa1f01da1a218856fdfd
lekhanhky <kykhanh2003@gmail.com>
2015-03-02 18:03:14 +0700

    lời lỗ doanh thu tồn kho thẻ kho

---
+++ BANHANG/BANHANG.DesktopClient/Screens/Xuất_Sản_PhẩmListDetail.lsml.vb
@@ -0,0 +1,56 @@
+﻿
+Namespace LightSwitchApplication
+
+    Public Class Xuất_Sản_PhẩmListDetail
+        Dim flagEdit As Boolean
+        Private Sub Xuất_Sản_PhẩmItemListAddAndEditNew_CanExecute(ByRef result As Boolean)
+            ' Write your code here.
+
+        End Sub
+
+        Private Sub Xuất_Sản_PhẩmItemListAddAndEditNew_Execute()
+            ' Write your code here.
+            Me.Xuất_Sản_Phẩm.AddNew()
+            Me.FindControl("Đối_TượngItem1").Focus()
+        End Sub
+
+        Private Sub OK_Execute()
+            ' Write your code here.
+            Me.CloseModalWindow("GroupChiTiet")
+        End Sub
+
+        Private Sub Hủy_Execute()
+            ' Write your code here.
+            If flagEdit Then
+                CType(Me.Xuất_Sản_Phẩm_Chi_Tiết.SelectedItem, XuatSanPhamChiTiet).Details.DiscardChanges()
+            Else
+                Me.Xuất_Sản_Phẩm_Chi_Tiết.DeleteSelected()
+            End If
+            Me.CloseModalWindow("GroupChiTiet")
+        End Sub
+
+        Private Sub Xuất_Sản_Phẩm_Chi_TiếtAddAndEditNew_CanExecute(ByRef result As Boolean)
+            ' Write your code here.
+
+        End Sub
+
+        Private Sub Xuất_Sản_Phẩm_Chi_TiếtAddAndEditNew_Execute()
+            ' Write your code here.
+            Me.Xuất_Sản_Phẩm_Chi_Tiết.AddNew()
+            Me.OpenModalWindow("GroupChiTiet") ' Write your code here.
+            Me.flagEdit = False
+        End Sub
+
+        Private Sub Xuất_Sản_Phẩm_Chi_TiếtEditSelected_CanExecute(ByRef result As Boolean)
+            ' Write your code here.
+
+        End Sub
+
+        Private Sub Xuất_Sản_Phẩm_Chi_TiếtEditSelected_Execute()
+            ' Write your code here.
+            Me.OpenModalWindow("GroupChiTiet") ' Write your code here.
+            Me.flagEdit = True
+        End Sub
+    End Class
+
+End Namespace
