93a43b6d0be0f3f9e37a58e2f95bd8675576fb59
lekhanhky <kykhanh2003@gmail.com>
2015-03-02 16:05:33 +0700

    Doanh thu !

---
+++ BANHANG/BANHANG.DesktopClient/Screens/vDoanh_Thu_NgàySearch.lsml.vb
@@ -0,0 +1,15 @@
+﻿
+Namespace LightSwitchApplication
+
+    Public Class vDoanh_Thu_NgàySearch
+
+        Private Sub vDoanh_Thu_NgàySearch_InitializeDataWorkspace(saveChangesTo As List(Of Microsoft.LightSwitch.IDataService))
+            Dim thang = Me.DataWorkspace.BanHangData.Tháng_Single(Today.Month)
+            Me.Tháng = thang ' Write your code here.
+            Dim nam = Me.DataWorkspace.BanHangData.Năm_Single(Today.Year)
+            Me.Năm = nam ' Write your code here.' Write your code here.
+
+        End Sub
+    End Class
+
+End Namespace
---
+++ BANHANG/BANHANG.DesktopClient/Screens/vDoanh_Thu_Ngày_Theo_Khách_HàngSearch.lsml.vb
@@ -0,0 +1,12 @@
+﻿
+Namespace LightSwitchApplication
+
+    Public Class vDoanh_Thu_Ngày_Theo_Khách_HàngSearch
+
+        Private Sub vDoanh_Thu_Ngày_Theo_Khách_HàngSearch_InitializeDataWorkspace(saveChangesTo As List(Of Microsoft.LightSwitch.IDataService))
+            ' Write your code here.
+
+        End Sub
+    End Class
+
+End Namespace
