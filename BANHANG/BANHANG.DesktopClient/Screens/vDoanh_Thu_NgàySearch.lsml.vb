366b71bb9b28f0fc4e38acdecc6370d31c80132f
lekhanhky <kykhanh2003@gmail.com>
2015-03-02 17:14:35 +0700

    lời lỗi - doanh thu !

--- BANHANG/BANHANG.DesktopClient/Screens/vDoanh_Thu_NgàySearch.lsml.vb
+++ BANHANG/BANHANG.DesktopClient/Screens/vDoanh_Thu_NgàySearch.lsml.vb
@@ -4,10 +4,8 @@ Namespace LightSwitchApplication
     Public Class vDoanh_Thu_NgàySearch
 
         Private Sub vDoanh_Thu_NgàySearch_InitializeDataWorkspace(saveChangesTo As List(Of Microsoft.LightSwitch.IDataService))
-            Dim thang = Me.DataWorkspace.BanHangData.Tháng_Single(Today.Month)
-            Me.Tháng = thang ' Write your code here.
-            Dim nam = Me.DataWorkspace.BanHangData.Năm_Single(Today.Year)
-            Me.Năm = nam ' Write your code here.' Write your code here.
+            Me.Từ_ngày = getNgayDauThang(Today) ' Write your code here.
+            Me.Đến_ngày = getNgayCuoiThang(Today) 'Write your code here.' Write your code here.
 
         End Sub
     End Class
---
+++ BANHANG/BANHANG.DesktopClient/Screens/vLời_Lỗ_NgàySearch.lsml.vb
@@ -0,0 +1,12 @@
+﻿
+Namespace LightSwitchApplication
+
+    Public Class vLời_Lỗ_NgàySearch
+
+        Private Sub vLời_Lỗ_NgàySearch_InitializeDataWorkspace(saveChangesTo As List(Of Microsoft.LightSwitch.IDataService))
+            Me.Từ_ngày = getNgayDauThang(Today) ' Write your code here.
+            Me.Đến_ngày = getNgayCuoiThang(Today)
+        End Sub
+    End Class
+
+End Namespace
