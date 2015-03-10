83ea9c72d4b28dcb3e0a5fcf0d74e88c8453732d
lekhanhky <kykhanh2003@gmail.com>
2015-03-10 11:09:38 +0700

    Revert "Revert "Chọn món add""
    This reverts commit c68aa276e7a9d4cee270c2160942df9041df0c6d.

---
+++ BANHANG/BANHANG.HTMLClient/Screens/ChonMonBrowse.lsml.js
@@ -0,0 +1,6 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+
+myapp.ChonMonBrowse.Món_postRender = function (element, contentItem) {
+    // Write code here.
+    lsWire.list.enableMultiSelect(contentItem);
+};
