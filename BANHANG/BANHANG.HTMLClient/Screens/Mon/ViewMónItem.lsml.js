116a33e11c67d9e458a5cdca359a8aa2c3a6a207
lekhanhky <kykhanh2003@gmail.com>
2015-03-10 15:19:46 +0700

    cập nhật món màn hình !

---
+++ BANHANG/BANHANG.HTMLClient/Screens/Mon/ViewMónItem.lsml.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+
+myapp.ViewMónItem.Details_postRender = function (element, contentItem) {
+    // Write code here.
+    var name = contentItem.screen.MónItem.details.getModel()[':@SummaryProperty'].property.name;
+    contentItem.dataBind("screen.MónItem." + name, function (value) {
+        contentItem.screen.details.displayName = value;
+    });
+}
+
