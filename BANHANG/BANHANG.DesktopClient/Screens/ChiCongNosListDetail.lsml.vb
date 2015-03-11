1efae52fd85b45192f08e7d04acc78bc295bc3b0
lekhanhky <kykhanh2003@gmail.com>
2015-03-12 00:08:07 +0700

    cập nhật thu chi công nợ ! (phải thu phải trả)

---
+++ BANHANG/BANHANG.DesktopClient/Screens/ChiCongNosListDetail.lsml.vb
@@ -0,0 +1,56 @@
+﻿
+Namespace LightSwitchApplication
+
+    Public Class ChiCongNosListDetail
+        Dim flagEdit As Boolean
+        Private Sub ChiCongNoListAddAndEditNew_CanExecute(ByRef result As Boolean)
+            ' Write your code here.
+
+        End Sub
+
+        Private Sub ChiCongNoListAddAndEditNew_Execute()
+            ' Write your code here.
+            Me.ChiCongNos.AddNew()
+            Me.FindControl("Đối_TượngItem1").Focus()
+        End Sub
+
+        Private Sub OK_Execute()
+            ' Write your code here.
+            Me.CloseModalWindow("GroupChiTiet")
+        End Sub
+
+        Private Sub Hủy_Execute()
+            ' Write your code here.
+            If flagEdit Then
+                CType(Me.Chi_Công_Nợ_Chi_Tiết.SelectedItem, ChiCongNoChiTiet).Details.DiscardChanges()
+            Else
+                Me.Chi_Công_Nợ_Chi_Tiết.DeleteSelected()
+            End If
+            Me.CloseModalWindow("GroupChiTiet")
+        End Sub
+
+        Private Sub Chi_Công_Nợ_Chi_TiếtAddAndEditNew_CanExecute(ByRef result As Boolean)
+            ' Write your code here.
+
+        End Sub
+
+        Private Sub Chi_Công_Nợ_Chi_TiếtAddAndEditNew_Execute()
+            ' Write your code here.
+            Me.Chi_Công_Nợ_Chi_Tiết.AddNew()
+            Me.OpenModalWindow("GroupChiTiet") ' Write your code here.
+            Me.flagEdit = False
+        End Sub
+
+        Private Sub Chi_Công_Nợ_Chi_TiếtEditSelected_CanExecute(ByRef result As Boolean)
+            ' Write your code here.
+
+        End Sub
+
+        Private Sub Chi_Công_Nợ_Chi_TiếtEditSelected_Execute()
+            ' Write your code here.
+            Me.OpenModalWindow("GroupChiTiet") ' Write your code here.
+            Me.flagEdit = True
+        End Sub
+    End Class
+
+End Namespace
---
+++ BANHANG/BANHANG.DesktopClient/Screens/ThuCongNosListDetail.lsml.vb
@@ -0,0 +1,56 @@
+﻿
+Namespace LightSwitchApplication
+
+    Public Class ThuCongNosListDetail
+        Dim flagEdit As Boolean
+        Private Sub ThuCongNoListAddAndEditNew_CanExecute(ByRef result As Boolean)
+            ' Write your code here.
+
+        End Sub
+
+        Private Sub ThuCongNoListAddAndEditNew_Execute()
+            ' Write your code here.
+            Me.ThuCongNos.AddNew()
+            Me.FindControl("Đối_TượngItem1").Focus()
+        End Sub
+
+        Private Sub OK_Execute()
+            ' Write your code here.
+            Me.CloseModalWindow("GroupChiTiet")
+        End Sub
+
+        Private Sub Hủy_Execute()
+            ' Write your code here.
+            If flagEdit Then
+                CType(Me.Thu_Công_Nợ_Chi_Tiết.SelectedItem, ThuCongNoChiTiet).Details.DiscardChanges()
+            Else
+                Me.Thu_Công_Nợ_Chi_Tiết.DeleteSelected()
+            End If
+            Me.CloseModalWindow("GroupChiTiet")
+        End Sub
+
+        Private Sub Thu_Công_Nợ_Chi_TiếtAddAndEditNew_CanExecute(ByRef result As Boolean)
+            ' Write your code here.
+
+        End Sub
+
+        Private Sub Thu_Công_Nợ_Chi_TiếtAddAndEditNew_Execute()
+            ' Write your code here.
+            Me.Thu_Công_Nợ_Chi_Tiết.AddNew()
+            Me.OpenModalWindow("GroupChiTiet") ' Write your code here.
+            Me.flagEdit = False
+        End Sub
+
+        Private Sub Thu_Công_Nợ_Chi_TiếtEditSelected_CanExecute(ByRef result As Boolean)
+            ' Write your code here.
+
+        End Sub
+
+        Private Sub Thu_Công_Nợ_Chi_TiếtEditSelected_Execute()
+            ' Write your code here.
+            Me.OpenModalWindow("GroupChiTiet") ' Write your code here.
+            Me.flagEdit = True
+        End Sub
+    End Class
+
+End Namespace
