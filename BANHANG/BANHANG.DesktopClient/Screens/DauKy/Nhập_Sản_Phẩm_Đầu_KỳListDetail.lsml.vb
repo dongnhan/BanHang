7d4a10d409cad82c313de19ff732daf42c999b56
lekhanhky <kykhanh2003@gmail.com>
2015-03-18 14:33:28 +0700

    cập nhật nhập đầu kỳ !

--- BANHANG/BANHANG.DesktopClient/Screens/DauKy/Nhập_Sản_Phẩm_Đầu_KỳListDetail.lsml.vb
+++ BANHANG/BANHANG.DesktopClient/Screens/DauKy/Nhập_Sản_Phẩm_Đầu_KỳListDetail.lsml.vb
@@ -2,7 +2,7 @@
 Namespace LightSwitchApplication
 
     Public Class Nhập_Sản_Phẩm_Đầu_KỳListDetail
-
+        Dim flagEdit As Boolean
         Private Sub Nhập_Sản_Phẩm_Đầu_KỳItemListAddAndEditNew_CanExecute(ByRef result As Boolean)
             ' Write your code here.
 
@@ -11,6 +11,44 @@ Namespace LightSwitchApplication
         Private Sub Nhập_Sản_Phẩm_Đầu_KỳItemListAddAndEditNew_Execute()
             Me.Nhập_Sản_Phẩm_Đầu_Kỳ.AddNew() ' Write your code here.
             Me.FindControl("Ghi_chú1").Focus()
+        End Sub
+
+        Private Sub OK_Execute()
+            ' Write your code here.
+            Me.CloseModalWindow("GroupChiTiet")
+        End Sub
+
+        Private Sub Hủy_Execute()
+            ' Write your code here.
+            If flagEdit Then
+                CType(Me.Nhập_Sản_Phẩm_Đầu_Kỳ_Chi_Tiết.SelectedItem, NhapSanPhamDauKyChiTiet).Details.DiscardChanges()
+            Else
+                Me.Nhập_Sản_Phẩm_Đầu_Kỳ_Chi_Tiết.DeleteSelected()
+            End If
+            Me.CloseModalWindow("GroupChiTiet")
+        End Sub
+
+        Private Sub Nhập_Sản_Phẩm_Đầu_Kỳ_Chi_TiếtAddAndEditNew_CanExecute(ByRef result As Boolean)
+            ' Write your code here.
+
+        End Sub
+
+        Private Sub Nhập_Sản_Phẩm_Đầu_Kỳ_Chi_TiếtAddAndEditNew_Execute()
+            ' Write your code here.
+            Me.Nhập_Sản_Phẩm_Đầu_Kỳ_Chi_Tiết.AddNew()
+            Me.OpenModalWindow("GroupChiTiet") ' Write your code here.
+            Me.flagEdit = False
+        End Sub
+
+        Private Sub Nhập_Sản_Phẩm_Đầu_Kỳ_Chi_TiếtEditSelected_CanExecute(ByRef result As Boolean)
+            ' Write your code here.
+
+        End Sub
+
+        Private Sub Nhập_Sản_Phẩm_Đầu_Kỳ_Chi_TiếtEditSelected_Execute()
+            ' Write your code here.
+            Me.OpenModalWindow("GroupChiTiet") ' Write your code here.
+            Me.flagEdit = True
         End Sub
     End Class
 
---
+++ BANHANG/BANHANG.Server/DataSources/BanHangData1/NhapSanPhamDauKyChiTiet.lsml.vb
@@ -0,0 +1,31 @@
+﻿
+Namespace LightSwitchApplication
+
+    Public Class NhapSanPhamDauKyChiTiet
+
+        Private Sub Sản_PhẩmItem_Validate(results As EntityValidationResultsBuilder)
+            Try
+                Dim dvt As Đơn_Vị_TínhItem1 = Me.DataWorkspace.BanHangData1.Đơn_Vị_Tính_Single(Me.Sản_PhẩmItem.Đơn_Vị_TínhItem.Id)
+                Me.Đơn_Vị_TínhItem = dvt
+            Catch ex As Exception
+
+            End Try ' results.AddPropertyError("<Error-Message>")
+
+        End Sub
+
+        Private Sub Thành_tiền_Compute(ByRef result As Decimal)
+            result = Số_lượng * Giá_nhập ' Set result to the desired field value
+
+        End Sub
+
+        Private Sub Tiền_thuế_Compute(ByRef result As Decimal)
+            result = Số_lượng * Giá_nhập * Phần_trăm_thuế ' Set result to the desired field value
+
+        End Sub
+
+        Private Sub Tổng_tiền_Compute(ByRef result As Decimal)
+            result = Thành_tiền + Tiền_thuế
+        End Sub
+    End Class
+
+End Namespace
