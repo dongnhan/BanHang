698c39bce88c3815a9865d9d58a42b4a8ee60964
lekhanhky <kykhanh2003@gmail.com>
2015-03-19 12:02:12 +0700

    thu công nợ !

--- BANHANG/BANHANG.DesktopClient/Screens/DauKy/Tiền_Mặt_Đầu_Kỳ1ListDetail.lsml.vb
+++ BANHANG/BANHANG.DesktopClient/Screens/DauKy/Tiền_Mặt_Đầu_Kỳ1ListDetail.lsml.vb
@@ -1,18 +1,19 @@
 ﻿
 Namespace LightSwitchApplication
 
-    Public Class Tiền_Mặt_Đầu_KỳListDetail
+    Public Class Tiền_Mặt_Đầu_Kỳ1ListDetail
 
-        Private Sub Tiền_Mặt_Đầu_KỳListDetail_InitializeDataWorkspace(saveChangesTo As List(Of Microsoft.LightSwitch.IDataService))
-            If Me.Tiền_Mặt_Đầu_Kỳ.SelectedItem IsNot Nothing Then
+        Private Sub Tiền_Mặt_Đầu_Kỳ1ListDetail_Activated()
+            If Me.Tiền_Mặt_Đầu_Kỳ1.SelectedItem IsNot Nothing Then
                 '0: ko cho xuất âm
                 '1: xuất âm
                 '2: xuất từ danh mục
                 'Me.Application.KiemTraTonKho = Me.Đơn_Vị_Sử_Dụng.SelectedItem.Kiểm_tra_tồn_kho
             Else
-                Me.Tiền_Mặt_Đầu_Kỳ.AddNew()
+                Me.Tiền_Mặt_Đầu_Kỳ1.AddNew()
                 Me.FindControl("Số_tiền1").Focus()
-            End If
+            End If ' Write your code here.
+
         End Sub
     End Class
 
---
+++ BANHANG/BANHANG.Server/DataSources/BanHangData/ThuCongNo.lsml.vb
@@ -0,0 +1,18 @@
+﻿
+Namespace LightSwitchApplication
+
+    Public Class ThuCongNo
+
+        Private Sub Số_tiền_Validate(results As EntityValidationResultsBuilder)
+            Dim tien = 0.0
+            For Each d In Thu_Công_Nợ_Chi_Tiết
+                tien += d.Số_tiền
+            Next
+            If tien > 0 Then
+                Số_tiền = tien
+            End If ' results.AddPropertyError("<Error-Message>")
+
+        End Sub
+    End Class
+
+End Namespace
