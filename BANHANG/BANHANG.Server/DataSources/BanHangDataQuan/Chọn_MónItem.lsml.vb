20add05c82e667e2b2591912ebd1e85ad3734ac4
lekhanhky <kykhanh2003@gmail.com>
2015-03-22 00:24:05 +0700

    chọn món computed ~!

---
+++ BANHANG/BANHANG.Server/DataSources/BanHangDataQuan/Chọn_MónItem.lsml.vb
@@ -0,0 +1,18 @@
+﻿
+Namespace LightSwitchApplication
+
+    Public Class Chọn_MónItem
+
+        Private Sub Thành_tiền_Compute(ByRef result As Decimal)
+            Dim tien = 0.0
+            For Each d In Chọn_Món_Chi_Tiết
+                tien += d.Thành_tiền
+            Next
+            If tien > 0 Then
+                result = tien
+            End If ' Set result to the desired field value
+
+        End Sub
+    End Class
+
+End Namespace
