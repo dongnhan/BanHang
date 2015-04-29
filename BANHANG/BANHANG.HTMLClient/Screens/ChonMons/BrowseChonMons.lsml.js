4189eb8479021165cdb89be78c977bb8603ef62e
lekhanhky <kykhanh2003@gmail.com>
2015-04-29 22:09:32 +0700

    tiếp tục

--- BANHANG/BANHANG.HTMLClient/Screens/ChonMons/BrowseChonMons.lsml.js
+++
@@ -1,6 +0,0 @@
-﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
-
-myapp.BrowseChonMons.TƯƠI_execute = function (screen) {
-    // Write code here.
-    window.location.reload();
-};
--- BANHANG/BANHANG.Server/Web/GetUserName.ashx.vb
+++ BANHANG/BANHANG.Server/Web/GetUserName.ashx.vb
@@ -5,14 +5,12 @@ Public Class GetUserName
     Implements System.Web.IHttpHandler
 
     Sub ProcessRequest(ByVal context As HttpContext) Implements IHttpHandler.ProcessRequest
-        Try
-            Dim servercontext = ServerApplicationContext.CreateContext()
 
-            context.Response.ContentType = "text/plain"
-            context.Response.Write(servercontext.Application.User.Name)
-        Catch ex As Exception
+        Dim servercontext = ServerApplicationContext.CreateContext()
 
-        End Try
+        context.Response.ContentType = "text/plain"
+        context.Response.Write(servercontext.Application.User.Name)
+
     End Sub
 
     ReadOnly Property IsReusable() As Boolean Implements IHttpHandler.IsReusable
