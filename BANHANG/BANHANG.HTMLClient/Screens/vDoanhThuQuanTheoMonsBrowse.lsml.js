5faebdb630bd47d4b8b29159055311c9728684f3
lekhanhky <kykhanh2003@gmail.com>
2015-05-03 18:09:50 +0700

    cập nhật danh mục, báo cáo !

---
+++ BANHANG/BANHANG.HTMLClient/Screens/vDoanhThuQuanTheoMonsBrowse.lsml.js
@@ -0,0 +1,11 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+
+myapp.vDoanhThuQuanTheoMonsBrowse.created = function (screen) {
+    // Write code here.
+    var tungay = new Date();
+    tungay.setHours(0, 0, 0, 0)
+    var denngay = new Date();
+    denngay.setHours(0, 0, 0, 0)
+    screen.Từ_ngày = tungay
+    screen.Đến_ngày = denngay
+};
